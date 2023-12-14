c9c0b54b0bb41ead3b0b593e4fcc9c8d45d3faad
jimzord12 <w.tool@yahoo.gr>
2023-12-14 16:14:40 +0200

    uuyyy

--- another_flutter_app/lib/main.dart
+++ another_flutter_app/lib/main.dart
@@ -64,16 +64,15 @@ class _MyHomePageState extends State<MyHomePage> {
                 buttonText: "Guild Game",
                 onPressed: () {
                   Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const GuildGameModuleWidget(
-                        role: 'professor',
-                        privKey:
-                            "2e0a0ee77631554530414c4a68385fd8328c837a1d5986f1e409f42d58a1a2f1",
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const GuildGameModuleWidget(
+                          role: 'professor',
+                          privKey:
+                              "2e0a0ee77631554530414c4a68385fd8328c837a1d5986f1e409f42d58a1a2f1",
+                        ),
                       ),
-                    ),
-                    (Route<dynamic> route) => false,
-                  );
+                      ModalRoute.withName(Navigator.defaultRouteName));
                 })
           ],
         ),
