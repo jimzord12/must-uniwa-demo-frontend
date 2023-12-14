6a74df4bebd3aab779f3a1f2d76961aec5dc90e6
jimzord12 <w.tool@yahoo.gr>
2023-12-14 18:32:50 +0200

    blabas78

--- guild_game_frontend/lib/screens/guild_board_screen.dart
+++ guild_game_frontend/lib/screens/guild_board_screen.dart
@@ -1,106 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:guild_game_frontend/navigation/go_back_button.dart'; // Import your CustomGoBackButton
 import 'package:guild_game_frontend/providers/quest_provider.dart';
 import 'package:guild_game_frontend/providers/user_provider.dart';
-import 'package:guild_game_frontend/widgets/modals/student/accept_quest_modal.dart';
-import 'package:guild_game_frontend/widgets/modals/student/view_completed_quest_modal.dart';
 import 'package:guild_game_frontend/widgets/stayros130/custom_button.dart';
 import 'package:provider/provider.dart';
 
 class GuildBoardScreen extends StatelessWidget {
-  const GuildBoardScreen({super.key});
+  const GuildBoardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final questProvider = Provider.of<QuestProvider>(context, listen: true);
     final UserProvider userProvider =
         Provider.of<UserProvider>(context, listen: true);
-    final List<dynamic> quests = userProvider.user!.ongoingQuests;
+    final List<dynamic> quests = questProvider.quests ?? [];
 
     double screenHeight = MediaQuery.of(context).size.height;
 
-    print("GuildBoardScreen - Rendered, ${questProvider.quests!.length}");
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Guild Board'),
-      ),
-      body: SingleChildScrollView(
-        // Wrap the Column in a SingleChildScrollView
-        child: Center(
-          child: Padding(
-            // Wrap the Column in a Padding widget
-            padding: const EdgeInsets.all(8.0), // Add padding here
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (quests.isEmpty) ...[
-                    SizedBox(height: screenHeight / 3),
-                    const Text(
-                      'There are no quests available at the moment',
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Color.fromARGB(127, 255, 255, 255),
-                        fontWeight: FontWeight.w400,
-                      ),
-                      textAlign: TextAlign.center,
-                    )
-                  ] else // questProvider.quests!.map((quest) {
-                    ...questProvider.quests!.map((quest) {
-                      return Padding(
-                        // Add padding to create a gap between CustomButton widgets
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        child: CustomButton(
-                            buttonText: quest.title,
-                            onPressed: () {
-                              if (userProvider.user!.role == 'student') {
-                                showAcceptQuestModal(
-                                  context: context,
-                                  walletAddress: userProvider.pubAddress!,
-                                  questId: quest.id!,
-                                  quest: quest,
-                                );
-                              } else {
-                                showQuestDetailsModal(
-                                  context: context,
-                                  walletAddress: userProvider.pubAddress!,
-                                  questId: quest.id!,
-                                  quest: quest,
-                                );
-                              }
-                            }),
-                      );
-                    }),
-                ]
-
-                // questProvider.quests!.map((quest) {
-                //   return Padding(
-                //     // Add padding to create a gap between CustomButton widgets
-                //     padding: const EdgeInsets.symmetric(vertical: 12.0),
-                //     child: CustomButton(
-                //         buttonText: quest.title,
-                //         onPressed: () {
-                //           if (userProvider.user!.role == 'student') {
-                //             showAcceptQuestModal(
-                //               context: context,
-                //               walletAddress: userProvider.pubAddress!,
-                //               questId: quest.id!,
-                //               quest: quest,
-                //             );
-                //           } else {
-                //             showQuestDetailsModal(
-                //               context: context,
-                //               walletAddress: userProvider.pubAddress!,
-                //               questId: quest.id!,
-                //               quest: quest,
-                //             );
-                //           }
-                //         }),
-                //   );
-                // }).toList(),
-
-                ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: SafeArea(
+              child: CustomGoBackButton(
+                icon: Icons.arrow_back,
+                iconColor: Colors.black,
+              ),
+            ),
           ),
-        ),
+          SingleChildScrollView(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top +
+                    48), // Adjust padding to prevent overlap
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: quests.isEmpty
+                      ? [
+                          SizedBox(height: screenHeight / 3),
+                          const Text(
+                            'There are no quests available at the moment',
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Color.fromARGB(127, 255, 255, 255),
+                              fontWeight: FontWeight.w400,
+                            ),
+                            textAlign: TextAlign.center,
+                          )
+                        ]
+                      : quests.map<Widget>((quest) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12.0),
+                            child: CustomButton(
+                              buttonText: quest.title,
+                              onPressed: () {
+                                // Handle button press based on role
+                              },
+                            ),
+                          );
+                        }).toList(),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ guild_game_frontend/lib/screens/guild_board_screen_OLD.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:guild_game_frontend/providers/quest_provider.dart';
+import 'package:guild_game_frontend/providers/user_provider.dart';
+import 'package:guild_game_frontend/widgets/modals/student/accept_quest_modal.dart';
+import 'package:guild_game_frontend/widgets/modals/student/view_completed_quest_modal.dart';
+import 'package:guild_game_frontend/widgets/stayros130/custom_button.dart';
+import 'package:provider/provider.dart';
+
+class GuildBoardScreen extends StatelessWidget {
+  const GuildBoardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final questProvider = Provider.of<QuestProvider>(context, listen: true);
+    final UserProvider userProvider =
+        Provider.of<UserProvider>(context, listen: true);
+    final List<dynamic> quests = userProvider.user!.ongoingQuests;
+
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    print(
+        "GuildBoardScreen - Rendered, Avail. Quests: ${questProvider.quests!.length}");
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Guild Board'),
+      ),
+      body: SingleChildScrollView(
+        // Wrap the Column in a SingleChildScrollView
+        child: Center(
+          child: Padding(
+            // Wrap the Column in a Padding widget
+            padding: const EdgeInsets.all(8.0), // Add padding here
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (quests.isEmpty) ...[
+                    SizedBox(height: screenHeight / 3),
+                    const Text(
+                      'There are no quests available at the moment',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Color.fromARGB(127, 255, 255, 255),
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  ] else // questProvider.quests!.map((quest) {
+                    ...questProvider.quests!.map((quest) {
+                      return Padding(
+                        // Add padding to create a gap between CustomButton widgets
+                        padding: const EdgeInsets.symmetric(vertical: 12.0),
+                        child: CustomButton(
+                            buttonText: quest.title,
+                            onPressed: () {
+                              if (userProvider.user!.role == 'student') {
+                                showAcceptQuestModal(
+                                  context: context,
+                                  walletAddress: userProvider.pubAddress!,
+                                  questId: quest.id!,
+                                  quest: quest,
+                                );
+                              } else {
+                                showQuestDetailsModal(
+                                  context: context,
+                                  walletAddress: userProvider.pubAddress!,
+                                  questId: quest.id!,
+                                  quest: quest,
+                                );
+                              }
+                            }),
+                      );
+                    }),
+                ]
+
+                // questProvider.quests!.map((quest) {
+                //   return Padding(
+                //     // Add padding to create a gap between CustomButton widgets
+                //     padding: const EdgeInsets.symmetric(vertical: 12.0),
+                //     child: CustomButton(
+                //         buttonText: quest.title,
+                //         onPressed: () {
+                //           if (userProvider.user!.role == 'student') {
+                //             showAcceptQuestModal(
+                //               context: context,
+                //               walletAddress: userProvider.pubAddress!,
+                //               questId: quest.id!,
+                //               quest: quest,
+                //             );
+                //           } else {
+                //             showQuestDetailsModal(
+                //               context: context,
+                //               walletAddress: userProvider.pubAddress!,
+                //               questId: quest.id!,
+                //               quest: quest,
+                //             );
+                //           }
+                //         }),
+                //   );
+                // }).toList(),
+
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+}
